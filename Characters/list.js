a69091740e2497609939e53d46d57f1374392c60
Sandra <sanmile@gmail.com>
2019-07-06 17:56:26 -0500

    Detalle de los personajes

--- Characters/list.js
+++ Characters/list.js
@@ -1,50 +1,51 @@
-fetch("https://rickandmortyapi.com/api/character",{
-    method: "GET"
-}).then((value) =>{
-    return value.json();
-}).then((value) => {
-    let listCharcter = document.getElementById("list-charcter");
-    console.log(value.results.length);
-    var nextItem = 1;
-    var loadCharcter = function(){
-        if(nextItem == 1)
-        {    
-            value.results.forEach(element => {
-                let div=document.createElement("div");        
-                div.className= "col-sm-4";
-                listCharcter.appendChild(div);
-                let charecterImg = document.createElement("img");
-                charecterImg.className="margin";
-                charecterImg.src = element.image;
-                div.appendChild(charecterImg);
+//import { detailCharter } from "..Detail/detail.js"
+
+var moduleList = (function () {
+    return {
+        getCaracter: function () {
+            let listCharcter = document.getElementById("list-charcter");            
+            var nextItem = 1;
+            var loadCharcter = function () {
+                fetch("https://rickandmortyapi.com/api/character/?page=" + nextItem, {
+                    method: "GET"
+                }).then((valueNext) => {
+                    return valueNext.json();
+                }).then((valueNext) => {
+                    valueNext.results.forEach(element => {
+                        let div = document.createElement("div");
+                        div.className = "col-sm-4";
+                        listCharcter.appendChild(div);
+                        let charecterImg = document.createElement("img");
+                        charecterImg.id = element.id;
+                        charecterImg.className = "margin";
+                        charecterImg.src = element.image;
+                        let character = document.createElement("a");
+                        character.href="../Detail/Detail.html?id=" + element.id;
+                       // character.addEventListener("click",function(e){ getDetails(e.target.id)} );
+                        character.append(charecterImg);
+                        div.appendChild(character);
+                    });
+                })
+                    .catch(function (error) { console.log(error); });
+            }
+
+            listCharcter.addEventListener('scroll', function () {
+                if (listCharcter.scrollTop + listCharcter.clientHeight >= listCharcter.scrollHeight) {
+                    nextItem++;
+                    loadCharcter();
+                }
             });
-        }else if(nextItem <= value.info.pages)
-        {
-            fetch("https://rickandmortyapi.com/api/character/?page=" + nextItem,{
-                method: "GET"
-            }).then((valueNext) =>{                
-                return valueNext.json();
-            }).then((valueNext) => {
-                valueNext.results.forEach(element => {
-                    let div=document.createElement("div");        
-                    div.className= "col-sm-4";
-                    listCharcter.appendChild(div);
-                    let charecterImg = document.createElement("img");
-                    charecterImg.className="margin";
-                    charecterImg.src = element.image;
-                    div.appendChild(charecterImg);
-                });   
-            })
-            .catch(function(error) { console.log(error); });
+
+            loadCharcter();
         }
     }
 
-    listCharcter.addEventListener('scroll', function() {
-        if (listCharcter.scrollTop + listCharcter.clientHeight >= listCharcter.scrollHeight) {
-            nextItem++;
-            loadCharcter();
-        }
-      });
+    function getDetails(id)
+    {
+        console.log(id);
+        
+    }
 
-      loadCharcter();
-})
+})();
+
+moduleList.getCaracter();
